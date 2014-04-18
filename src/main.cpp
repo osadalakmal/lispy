1b17a894dc055e0046b7a87ff25d27930ed5ce69
Osada Paranaliyanage <osadalakmal@gmail.com>
2014-04-18 23:43:52 +0100

    chapter 6 done

--- src/main.cpp
+++ src/main.cpp
@@ -13,25 +13,28 @@ class MpcParser {
 	std::shared_ptr<mpc_parser_t> d_parser;
 public:
 	MpcParser(const std::string& name) : d_parser(mpc_new(name.c_str()), std::bind(&mpc_cleanup,1,std::placeholders::_1)) { }
+	mpc_parser_t* get() { return d_parser.get(); }
 };
 
 int main(int argc, char** argv) {
 
 	/* Create Some Parsers */
-	mpc_parser_t* Number = mpc_new("number");
-	mpc_parser_t* Operator = mpc_new("operator");
-	mpc_parser_t* Expr = mpc_new("expr");
-	mpc_parser_t* Lispy = mpc_new("lispy");
+	MpcParser Number("number");
+	MpcParser RealNum("real_num");
+	MpcParser Operator("operator");
+	MpcParser Expr("expr");
+	MpcParser Lispy("lispy");
 
 	/* Define them with the following Language */
 	mpca_lang(MPCA_LANG_DEFAULT,
-			"                                                     \
+			"                                               \
+		real_num : /-?[0-9]*\\.[0-9]+/ ;                    \
 	    number   : /-?[0-9]+/ ;                             \
-	    operator : '+' | '-' | '*' | '/' ;                  \
-	    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+	    operator : '+' | '-' | '*' | '/' | '%' ;            \
+	    expr     : <real_num> | <number> | '(' <operator> <expr>+ ')' ;  \
 	    lispy    : /^/ <operator> <expr>+ /$/ ;             \
 	  ",
-	  Number, Operator, Expr, Lispy);
+	  RealNum.get(), Number.get(), Operator.get(), Expr.get(), Lispy.get());
 
 	/* Print Version and Exit Information */
 	puts("Lispy Version 0.0.0.0.1");
@@ -46,16 +49,24 @@ int main(int argc, char** argv) {
 		/* Add input to history */
 		add_history(input);
 
-		/* Echo input back to user */
-		printf("No you're a %s\n", input);
+		if (!input)
+			break;
+
+		mpc_result_t r;
+		if (mpc_parse("<stdin>", input, Lispy.get(), &r)) {
+		  /* On Success Print the AST */
+		  mpc_ast_print((mpc_ast_t*)r.output);
+		  mpc_ast_delete((mpc_ast_t*)r.output);
+		} else {
+		  /* Otherwise Print the Error */
+		  mpc_err_print(r.error);
+		  mpc_err_delete(r.error);
+		}
 
 		/* Free retrived input */
 		free(input);
 
 	}
-
-	/* Undefine and Delete our Parsers */
-	mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
 	return 0;
 }
