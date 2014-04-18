0e5f7b7253a1082fdbbc986ee178ef396c57da18
Osada Paranaliyanage <osadalakmal@gmail.com>
2014-04-18 22:49:57 +0100

    added experimental support for deps

--- Makefile
+++ Makefile
@@ -5,30 +5,36 @@ C_INCLUDE_PATH=$(shell echo "/usr/include/$(gcc -print-multiarch)")
 CPLUS_INCLUDE_PATH=$(shell echo "/usr/include/$(gcc -print-multiarch)")
 LDFLAGS := -lreadline
 LD = g++
-CFLAGS = -g -Wall
-CPPFLAGS = -g -Wall
+CFLAGS = -g -Wall -I./
+CPPFLAGS = -g -Wall -I./ --std=c++0x
 RM = /bin/rm -f
 SRCS = $(wildcard src/*.cpp) $(wildcard src/*.c)  
 OBJS = $(patsubst %.cpp,%.o,$(patsubst %.c,%.o ,$(SRCS)))
+DEPS = $(patsubst %d,%o,$(OBJS))
 PROG = lispy.tsk
 
 all: $(PROG)
 
-$(PROG): $(OBJS)
+$(PROG): $(OBJS) $(DEPS)
 	$(LD) $(OBJS) -o $(PROG) $(LDFLAGS)
 
 %.o: %.cpp
 	$(CXX) $(CPPFLAGS) -c $< -o $@
 	
-%.o: %.cpp
+%.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
-
+	
+%.d: %.c
+	@set -e; rm -f $@; \
+	$(CC) -M $(CFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+	
+%.d: %.cpp
+	@set -e; rm -f $@; \
+	$(CXX) -M $(CPPFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
+	rm -f $@.$$$$
+	
 clean:
 	$(RM) $(PROG) $(OBJS)
-
-depend:
-	$(RM) .depend
-	@echo $(LIBRARY_PATH) $(C_INCLUDE_PATH) $(CPLUS_INCLUDE_PATH)
-	makedepend -f- -- $(CFLAGS) -- $(SRCS) > .depend
-        
-include .depend
--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "mpc.h"
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <string>
+#include <functional>
+#include <memory>
+
+class MpcParser {
+	std::shared_ptr<mpc_parser_t> d_parser;
+public:
+	MpcParser(const std::string& name) : d_parser(mpc_new(name.c_str()), std::bind(&mpc_cleanup,1,std::placeholders::_1)) { }
+};
 
 int main(int argc, char** argv) {
 
-  /* Print Version and Exit Information */
-  puts("Lispy Version 0.0.0.0.1");
-  puts("Press Ctrl+c to Exit\n");
+	/* Create Some Parsers */
+	mpc_parser_t* Number = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr = mpc_new("expr");
+	mpc_parser_t* Lispy = mpc_new("lispy");
 
-  /* In a never ending loop */
-  while (1) {
+	/* Define them with the following Language */
+	mpca_lang(MPCA_LANG_DEFAULT,
+			"                                                     \
+	    number   : /-?[0-9]+/ ;                             \
+	    operator : '+' | '-' | '*' | '/' ;                  \
+	    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+	    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+	  ",
+	  Number, Operator, Expr, Lispy);
 
-    /* Output our prompt and get input */
-    char* input = readline("lispy> ");
+	/* Print Version and Exit Information */
+	puts("Lispy Version 0.0.0.0.1");
+	puts("Press Ctrl+c to Exit\n");
 
-    /* Add input to history */
-    add_history(input);
+	/* In a never ending loop */
+	while (1) {
 
-    /* Echo input back to user */
-    printf("No you're a %s\n", input);
+		/* Output our prompt and get input */
+		char* input = readline("lispy> ");
 
-    /* Free retrived input */
-    free(input);
+		/* Add input to history */
+		add_history(input);
 
-  }
+		/* Echo input back to user */
+		printf("No you're a %s\n", input);
 
-  return 0;
+		/* Free retrived input */
+		free(input);
+
+	}
+
+	/* Undefine and Delete our Parsers */
+	mpc_cleanup(4, Number, Operator, Expr, Lispy);
+
+	return 0;
 }
